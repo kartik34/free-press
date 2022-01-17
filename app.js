b300e0be64f76db840d5c29588d6c2e1f6aec163
Kartik Singhal <kartiksinghal3.14@gmail.com>
2022-01-16 19:06:23 -0500

    clean up and format code

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var newsRouter = require('./routes/news');
 
 var app = express();
@@ -21,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/news', newsRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
